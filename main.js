9ebd8bcee86ec47739546e4e1f8795d7c27fae7b
Henry Lee <104782108+ktsnv@users.noreply.github.com>
2024-11-07 10:08:58 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 function setup() {
   let person = prompt("Please enter your name", "");
   let stand = prompt("Please enter your stand's name", "");
-  let standPowers = ["a","b","c","d"]
+  let standPowers = [
+    "Fire Manipulation",
+    "Ice Manipulation",
+    "Water Manipulation",
+    "Air Manipulation",
+    "Ice Manipulation",
+    "Rain Manipulation",
+    "Calamity Manipulation",
+    "Dinosaur Transformation",
+    "Sound Manipulation",
+    "Blood Manipulation",
+    "Mind Control",
+    "Time Stop",
+    "Time Reversal",
+    "Time Acceleration",
+    "Time Erase"
+  ]
   createElement("p", "Stand User: " + person)
   createElement("p", "Stand Name: " + stand)
   createElement("p", "Stand Power: " + generateRandom(standPowers))
