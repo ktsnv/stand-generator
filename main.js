365dfafd60c382d4d004e6547b4d33c886b76e3f
Henry Lee <104782108+ktsnv@users.noreply.github.com>
2024-12-06 11:13:21 -0600

    Update main.js

--- main.js
+++ main.js
@@ -216,7 +216,7 @@ function createChart()
     // color alternating cells
     chart.yGrid().palette(['Black', 'Black 0']);
     chart.fontSize(25);
-    chart.area(data).name('Stand').markers(true).fill('#FF69B4', 0.3).stroke('#E55934', 0);
+    chart.area(data).name('Stand').fill('#FF69B4', 0.3).stroke('#E55934', 0);
     chart.legend(false);
     chart.background().fill("red", 0);
     // set container id for the chart
