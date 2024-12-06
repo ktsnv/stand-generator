1f232b39ec4c424c5623a772a7e9b65eca04aa4b
Henry Lee <104782108+ktsnv@users.noreply.github.com>
2024-12-06 11:20:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -214,8 +214,8 @@ function createChart()
       .ticks({'interval':1});
 
     // color alternating cells
-    chart.yGrid().palette(['Black 0', 'Black 0']);
-    chart.area(data).name('Stand').fill('#FF69B4', 0.3).stroke('#E55934', 0);
+    chart.yGrid().palette('Black 0');
+    chart.area(data).name('Stand').fill('#FF69B4', 0.75).stroke('#E55934', 0);
     chart.legend(false);
     chart.background().fill("red", 0);
     chart.labels().fontSize(25);
